b73cc3565269180036f8dfb8ae6e9f06ab16bd63
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-06-28 01:04:52 -0400

    got rid of login page, no longer neaded all info in landing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./components/landing";
-import LoginPage from "./components/loginpage";
 import SignUp from "./components/signup";
 import About from "./components/about";
 import Todo from "./components/todo";
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/home" element={<Todo />} />
         <Route path="/about" element={<About />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Landing />} />
       </Routes>
