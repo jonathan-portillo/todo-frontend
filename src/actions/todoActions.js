66a612adf6e5901b7fe9d4ec1396297035442adc
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-06-28 23:43:03 -0400

    updated actions

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -2,6 +2,7 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 export const ADD_NEW_USER = "ADD_NEW_USER";
 export const ADD_NEW_USER_SUCCESS = "ADD_NEW_USER_SUCCESS";
 export const ADD_NEW_USER_FAIL = "ADD_NEW_USER_FAIL";
+export const HANDLE_CHANGE_SIGNUP = "HANDLE_CHANGE_SIGNUP";
 
 export const createUser = (newUser) => (dispatch) => {
   dispatch({ type: ADD_NEW_USER });
@@ -10,9 +11,10 @@ export const createUser = (newUser) => (dispatch) => {
     .then((res) => {
       dispatch({
         type: ADD_NEW_USER_SUCCESS,
-        payload: res,
+        payload: res.data,
       });
       console.log(res);
+      localStorage.setItem("token", res.data.token);
       localStorage.setItem("id", res.data.id);
     })
     .catch((err) => {
@@ -21,4 +23,8 @@ export const createUser = (newUser) => (dispatch) => {
         payload: "Sign Up Failed try again!",
       });
     });
+};
+
+export const handle_change_signup = (e) => (dispatch) => {
+  dispatch({ type: HANDLE_CHANGE_SIGNUP, payload: e });
 };
