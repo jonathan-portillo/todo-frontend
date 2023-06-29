2a49d871104d33b255a83307c5265f82f8e81f5e
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-06-29 01:08:50 -0400

    created my log in reducers

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,4 +1,9 @@
-import { ADD_NEW_USER, HANDLE_CHANGE_SIGNUP } from "../actions/todoActions";
+import {
+  ADD_NEW_USER,
+  HANDLE_CHANGE_SIGNUP,
+  LOGIN,
+  HANDLE_CHANGE_LOGIN,
+} from "../actions/todoActions";
 
 const initialState = {
   signUpForm: {
@@ -17,11 +22,23 @@ export const todoReducer = (state = initialState, action) => {
       return {
         ...state,
       };
+    case LOGIN:
+      return {
+        ...state,
+      };
     case HANDLE_CHANGE_SIGNUP:
       return {
         ...state,
         signUpForm: {
           ...state.signUpForm,
+          [action.payload.target.name]: action.payload.target.value,
+        },
+      };
+    case HANDLE_CHANGE_LOGIN:
+      return {
+        ...state,
+        loginForm: {
+          ...state.loginForm,
           [action.payload.target.name]: action.payload.target.value,
         },
       };
