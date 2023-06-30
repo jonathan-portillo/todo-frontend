3a8a9a7b41246dfb3d84d208d87ac8bb7d15188a
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-06-30 02:05:11 -0400

    fixed function

--- src/utils/privateroute.js
+++ src/utils/privateroute.js
@@ -4,9 +4,9 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
   const userAuthenticated = localStorage.getItem("token");
   if (!userAuthenticated) {
-    return children;
+    return <Navigate to="/" />;
   }
-  return <Navigate to="/" />;
+  return children;
 };
 
 export default PrivateRoute;
