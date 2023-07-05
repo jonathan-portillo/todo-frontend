1885171bc2eafb8f0eb8501c9778017ad1273fb9
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-07-05 00:24:24 -0400

    connected task to state

--- src/components/task.js
+++ src/components/task.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import Nav from "../components/nav";
 import NewTask from "./newtask";
+import CurrentTasks from "./currenttask";
+import { connect } from "react-redux";
+import { createNewTask, handle_change_newtask } from "../actions/todoActions";
 
 const Task = () => {
   return (
     <>
       <Nav />
-      <NewTask />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="dashbox">
+        <NewTask />
+        <br />
+        <br />
+        <br />
+        <CurrentTasks />
+      </div>
     </>
   );
 };
 
-export default Task;
+const mapStateToProps = (state) => {
+  return {
+    newTask: state.newTask,
+  };
+};
+
+export default connect(mapStateToProps, {
+  createNewTask,
+  handle_change_newtask,
+})(Task);
