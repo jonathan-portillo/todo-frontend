dc78e6bbe7eb63eadd2952af0d08663f5294416b
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-07-05 02:01:37 -0400

    connected component to props to load all tasks created by user

--- src/components/currenttask.js
+++ src/components/currenttask.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getTasks } from "../actions/todoActions";
+import { connect } from "react-redux";
 
-const CurrentTasks = () => {
-  return <div>All my notes are here </div>;
+const CurrentTasks = (props) => {
+  useEffect(() => {
+    props.getTasks(props.allTasks);
+  }, []);
+
+  console.log("where info is stored", props.allTasks);
+
+  return (
+    <>
+      <div>All my notes are here </div>
+      <br />
+      <div>
+        {props.allTasks.map((res) => {
+          return res.map((res) => {
+            return <p key={res.id}>{res.todo_title}</p>;
+          });
+        })}
+      </div>
+    </>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    allTasks: state.allTasks,
+  };
 };
 
-export default CurrentTasks;
+export default connect(mapStateToProps, {
+  getTasks,
+})(CurrentTasks);
