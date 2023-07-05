0cd34bb2dbb86ba2be04ca2ef9bc815ad1ca4421
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-07-05 00:22:59 -0400

    new task actions created

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,13 +1,22 @@
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 export const ADD_NEW_USER = "ADD_NEW_USER";
 export const ADD_NEW_USER_SUCCESS = "ADD_NEW_USER_SUCCESS";
 export const ADD_NEW_USER_FAIL = "ADD_NEW_USER_FAIL";
+
 export const HANDLE_CHANGE_SIGNUP = "HANDLE_CHANGE_SIGNUP";
 export const HANDLE_CHANGE_LOGIN = "HANDLE_CHANGE_LOGIN";
+export const HANDLE_CHANGE_NEW_TASK = "HANDLE_CHANGE_NEW_TASK";
+
 export const LOGIN = "LOGIN";
 export const LOGIN_SUCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 
+export const NEW_TASK = "NEW_TASK";
+export const NEW_TASK_SUCCESS = "NEW_TASK_SUCCESS";
+export const NEW_TASK_FAIL = "NEW_TASK_FAIL";
+
+//SIGN UP
 export const createUser = (newUser) => (dispatch) => {
   dispatch({ type: ADD_NEW_USER });
   axiosWithAuth()
@@ -29,6 +38,7 @@ export const createUser = (newUser) => (dispatch) => {
     });
 };
 
+//LOGIN
 export const logUserIn = (cred) => (dispatch) => {
   dispatch({ type: LOGIN });
   axiosWithAuth()
@@ -48,10 +58,32 @@ export const logUserIn = (cred) => (dispatch) => {
     });
 };
 
+//Create a new task
+
+export const createNewTask = (task) => (dispatch) => {
+  dispatch({ type: NEW_TASK });
+  axiosWithAuth()
+    .post(`/title/user/${localStorage.getItem("id")}`, task)
+    .then((res) => {
+      dispatch({ type: NEW_TASK_SUCCESS, payload: res.data });
+      console.log(res.data);
+    })
+    .catch((err) => {
+      dispatch({
+        type: NEW_TASK_FAIL,
+        payload: "Not sending title please help please",
+      });
+    });
+};
+
 export const handle_change_login = (e) => (dispatch) => {
   dispatch({ type: HANDLE_CHANGE_LOGIN, payload: e });
 };
 
 export const handle_change_signup = (e) => (dispatch) => {
   dispatch({ type: HANDLE_CHANGE_SIGNUP, payload: e });
+};
+
+export const handle_change_newtask = (e) => (dispatch) => {
+  dispatch({ type: HANDLE_CHANGE_NEW_TASK, payload: e });
 };
