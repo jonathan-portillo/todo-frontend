52f4c491365fb82d23a26a4998aebfe94c22153a
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-07-19 23:59:53 -0400

    moving login to its own page

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Login from "./login";
-import { Button } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import LockedOutIcon from "@mui/icons-material/Assignment";
 import { Avatar } from "@mui/material";
 import Nav from "../components/nav";
+import Footer from "../components/footer";
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const signup = (e) => {
+    e.preventDefault();
+    navigate("/signup");
+  };
+
   return (
     <>
       <Nav />
-      <div className="homebox">
-        <div className="mottobox">
-          <p className="thatsthemotto">Task Notes</p>
-          <p className="thatsthemotto2">Empower your productivity</p>
-        </div>
+      <div className="landing">
+        <div className="homebox">
+          <div className="mottobox">
+            <p className="thatsthemotto">Task Note</p>
+            <p className="thatsthemotto2">Empower your productivity</p>
+            <Button onClick={signup} variant="text">
+              Get Started
+            </Button>
+          </div>
 
-        <div className="loginlanding">
+          {/* <div className="loginlanding">
           <Paper className="loginboxlanding">
             <div className="avatar">
               <Avatar
@@ -30,7 +41,9 @@ const Landing = () => {
             <p>Welcome Back!</p>
             <Login />
           </Paper>
+        </div> */}
         </div>
+        <Footer />
       </div>
     </>
   );
