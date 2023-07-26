5171079665e04be37d75012e6783fc6bd5de3335
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-07-25 22:12:15 -0400

    added an add button and a trash button

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,7 +1,36 @@
 import React from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import IconButton from "@mui/material/IconButton";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const SideNav = () => {
-  return <p>This is our side nav</p>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3F2305",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="sidenavbox">
+        <IconButton color="primary">
+          <AddCircleIcon
+            style={{ fontSize: 50 }}
+            aria-label="create a new task"
+          />
+        </IconButton>
+        <br />
+        <IconButton color="primary">
+          <RemoveCircleIcon style={{ fontSize: 50 }} aria-label="delete tasks">
+            Delete Tasks
+          </RemoveCircleIcon>
+        </IconButton>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default SideNav;
--- src/components/task.js
+++ src/components/task.js
@@ -4,21 +4,18 @@ import NewTask from "./newtask";
 import CurrentTasks from "./currenttask";
 import { connect } from "react-redux";
 import { createNewTask, handle_change_newtask } from "../actions/todoActions";
+import SideNav from "./sidenav";
 
 const Task = () => {
   return (
     <>
       <Nav />
-      <br />
-      <br />
-      <br />
-      <br />
       <div className="dashbox">
-        <NewTask />
-        <br />
-        <br />
-        <br />
-        <CurrentTasks />
+        <SideNav />
+        <div className="notebox">
+          <NewTask />
+          <CurrentTasks />
+        </div>
       </div>
     </>
   );
