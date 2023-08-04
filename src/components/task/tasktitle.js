619e9d8fe39203b89b2ecfcafc304afd57b05564
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-08-03 22:14:27 -0400

    conditional rendering for buttons and input for updating task titles

--- src/components/task/tasktitle.js
+++ src/components/task/tasktitle.js
@@ -1,39 +1,76 @@
 import React from "react";
-import { getTasks, deleteTask, updateTask } from "../../actions/todoActions";
 import { connect } from "react-redux";
+import {
+  deleteTask,
+  updateTask,
+  setEditing,
+  editTitle,
+} from "../../actions/todoActions";
 
 const TaskTitle = (props) => {
+  const {
+    task,
+    deleteTask,
+    updateTask,
+    isEditing,
+    editedTitle,
+    setEditing,
+    editTitle,
+  } = props;
+
+  const handleEditClick = () => {
+    editTitle(task.todo_title); // Set the editedTitle in Redux state
+    setEditing(task.id, true); // Set the isEditing for this specific task
+  };
+
+  const handleSaveClick = async (e) => {
+    e.preventDefault();
+    console.log("Task ID to be updated:", task.id);
+    console.log("Updated Title:", editedTitle);
+
+    await updateTask(task.id, { todo_title: editedTitle }); // Update the task title
+    setEditing(task.id, false); // Disable editing for this task
+  };
+
   return (
     <>
-      <input type="checkbox" className="toggle" id={`task_${props.task.id}`} />
+      <input type="checkbox" className="toggle" id={`task_${task.id}`} />
       <div className="tasktitlebox">
-        <label className="tasktitle" htmlFor={`task_${props.task.id}`}>
-          {props.task.todo_title}
-        </label>
+        {isEditing ? (
+          <input
+            type="text"
+            value={editedTitle}
+            onChange={(e) => editTitle(e.target.value)} // Update the editedTitle in Redux state
+          />
+        ) : (
+          <label className="tasktitle" htmlFor={`task_${task.id}`}>
+            {task.todo_title}
+          </label>
+        )}
         <div className="buttonsss">
-          <button>Edit</button>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              props.deleteTask(props.task.id);
-            }}
-          >
-            Delete
-          </button>
+          {isEditing ? (
+            <button onClick={handleSaveClick}>Save</button>
+          ) : (
+            <button onClick={handleEditClick}>Edit</button>
+          )}
+          <button onClick={() => deleteTask(task.id)}>Delete</button>
         </div>
       </div>
     </>
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  const taskId = ownProps.task.id;
   return {
-    allTasks: state.allTasks,
+    editedTitle: state.editedTitle,
+    isEditing: state.isEditing[taskId] || false,
   };
 };
 
 export default connect(mapStateToProps, {
-  getTasks,
   deleteTask,
   updateTask,
+  setEditing,
+  editTitle,
 })(TaskTitle);
