0c60f364a178975cc06c774c4af68e1a9f9e7169
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-10-03 01:11:06 -0400

    send button

--- src/components/notes/newNote.js
+++ src/components/notes/newNote.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import {
   handle_change_note_description,
   createNewNote,
 } from "../../actions/todoActions";
 import { TextField } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import Button from "@mui/material/Button";
+import * as yup from "yup";
 
 const NewNote = (props) => {
+  const [isValid, setIsValid] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     await props.createNewNote(props.id, {
       todo_list: props.newNoteDescription,
     });
+  };
+
+  const validationSchema = yup.object().shape({
+    todo_list: yup.string().required("Note cannot be empty"), // Require a non-empty note
+  });
+
+  const checkFormValidity = async (value) => {
+    try {
+      await validationSchema.validate(value);
+      setIsValid(true);
+    } catch (error) {
+      setIsValid(false);
+    }
   };
 
   return (
@@ -26,10 +44,21 @@ const NewNote = (props) => {
           defaultValue="Type in your note"
           style={{ height: "100%" }}
           value={props.newNoteDescription.todo_list}
-          onChange={(e) => props.handle_change_note_description(e.target.value)}
+          onChange={(e) => {
+            props.handle_change_note_description(e.target.value);
+            checkFormValidity({ todo_list: e.target.value });
+          }}
           className="notepad-input"
         />
-        <button type="submit">Submit</button>
+        <Button
+          disabled={!isValid}
+          type="submit"
+          variant="contained"
+          endIcon={<SendIcon />}
+          style={{ padding: "2px 4px", fontSize: "8px" }}
+        >
+          Send
+        </Button>
       </form>
     </>
   );
